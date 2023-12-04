c900eaa38f8e4c7ae14087b1891b11613bb2d966
GabrielMarconatto-terra <gabriel.marconatto_compasso@terrainvestimentos.com.br>
2023-12-04 10:33:14 -0300

    first commit

---
+++ src/test/java/Monitor/demo/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package Monitor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
